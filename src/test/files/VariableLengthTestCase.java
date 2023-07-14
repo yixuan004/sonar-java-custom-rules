ad76c0257fc3f2f3f62393eeedb65de2f9693058
yixuan004 <004liuyixuan@gmail.com>
2023-07-14 15:43:53 +0800

    添加规则 1. 尝试添加规则：变量名称长度需要<20，并走通一次自定义规则的流程，补充TesCase;

---
+++ src/test/files/VariableLengthTestCase.java
@@ -0,0 +1,14 @@
+package hihi.hahaha.hhhh;
+
+/**
+ * Created by Liu Yixuan on 2023/7/13.
+ */
+public class VariableLengthTestCase {
+    public int abcdefghijklmnopqrst;  // Noncompliant
+    public String abcdefghijklmnopqrs;
+
+
+    public void tsrqponmlkjihgfedcba(String a) {
+        System.out.println("Hello, World");
+    }
+}
